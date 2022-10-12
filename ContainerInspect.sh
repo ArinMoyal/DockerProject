b4deac40514f3d15502ee2034da6ca50037507d9
ArinMoyal <108821356+ArinMoyal@users.noreply.github.com>
2022-10-12 14:13:43 +0300

    Added -d for detached

--- ContainerInspect.sh
+++ ContainerInspect.sh
@@ -3,5 +3,5 @@ if [ "$( docker container inspect -f '{{.State.Status}}' AlpCon )" == "running" 
 else
 	echo "AlpCon not running, starting AlpCon"
 	docker rm -f AlpCon
-	docker run -it --name AlpCon alpcon
+	docker run -d -it --name AlpCon alpcon
 fi
