1e740f1760c3bdf15c30e565b8f8843b27ecc591
ArinMoyal <108821356+ArinMoyal@users.noreply.github.com>
2022-10-18 16:54:37 +0300

    Update cube.py

--- cubecalculation/cube.py
+++ cubecalculation/cube.py
@@ -9,8 +9,8 @@ def cube_calculations(numbers):
         result_a.append(math_library.sqrt(num ** 3))
 
 if __name__ == "__main__":
-    # number_list = list(range(5000000))
-    number_list = list(range(20000000))
+    number_list = list(range(5000000))
+    # number_list = list(range(20000000))
     start_time = time_library.time()
     cube_calculations(number_list)
     end_time = time_library.time()
