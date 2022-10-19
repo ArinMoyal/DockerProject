1c3cdf05a66a028f92f6e6703cc87796c20b001a
ArinMoyal <108821356+ArinMoyal@users.noreply.github.com>
2022-10-19 14:27:10 +0300

    imported sys and added output to file

--- simple/simple.py
+++ simple/simple.py
@@ -1,4 +1,7 @@
 import time
+import sys
 print("Hello, user!")
 time.sleep(5)
-print("It's been 5 seconds!")
+with open('/simple/results', 'w') as f:
+        sys.stdout = f
+        print("It's been 5 seconds!")
