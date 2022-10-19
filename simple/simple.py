39855d5c7d7c446de33152e7c82d33a06c513bdd
ArinMoyal <108821356+ArinMoyal@users.noreply.github.com>
2022-10-19 14:46:23 +0300

    Update simple.py

--- simple/simple.py
+++ simple/simple.py
@@ -1,7 +1,7 @@
 import time
 import sys
 print("Hello, user!")
-time.sleep(11)
+time.sleep(9)
 with open('/simple/results', 'w') as f:
         sys.stdout = f
-        print("It's been 11 seconds!")
+        print("It's been 9 seconds!")
